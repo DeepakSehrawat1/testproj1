926e5a577219bac319248e537d9c3a2755d02c72
DeepakSehrawat1 <deepaksehrawat150@gmail.com>
2024-01-28 21:06:15 +0530

    project

---
+++ public/script.js
@@ -0,0 +1,6 @@
+function showForm(boxId) {
+  // You can customize this function to show the form next to the clicked box
+  document.getElementById("form-container").style.display = "block";
+}
+
+console.log("lovw");
